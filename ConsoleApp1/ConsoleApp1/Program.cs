65386b913b213e569b426d26c0c60e6663d48659
DeimosIX <kujkin999@yandex.ru>
2020-12-28 23:26:56 +0300

    Добавление цикла

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,42 +6,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Консольный калькулятор (версия 1.0)");
-            Console.WriteLine(@"Выберите арифметическое действие:
+            while (true)
+            {
+                //Console.WriteLine("Консольный калькулятор (версия 1.0)");
+                Console.WriteLine(@"Выберите арифметическое действие:
             - Умножение (введите 1)
             - Деление (введите 2)
             - Сложение (введите 3)
             - Вычитание (введите 4)");
-            string q = Console.ReadLine();
-            double a, b;
-            Console.WriteLine(' ');
-            Console.WriteLine("Введите первое значение");
-            a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите второе значение");
-            b = double.Parse(Console.ReadLine());
-            Console.WriteLine(' ');
-            if (q == "1")
-            {
-                Console.WriteLine("Результат умножения = {0}", a * b);
-            }
-            if (q == "2")
-            {
-                if (b == 0)
+                string q = Console.ReadLine();
+                double a, b;
+                Console.WriteLine(' ');
+                Console.WriteLine("Введите первое значение");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе значение");
+                b = double.Parse(Console.ReadLine());
+                Console.WriteLine(' ');
+                if (q == "1")
                 {
-                    Console.WriteLine("Ошибка деления на ноль");
+                    Console.WriteLine("Результат умножения = {0}", a * b);
                 }
-                else
+                if (q == "2")
                 {
-                    Console.WriteLine("Результат деления = {0}", a / b);
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Ошибка деления на ноль");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Результат деления = {0}", a / b);
+                    }
                 }
-            }
-            if (q == "3")
-            {
-                Console.WriteLine("Результат сложения = {0}", a + b);
-            }
-            if (q == "4")
-            {
-                Console.WriteLine("Результат вычитания = {0}", a - b);
+                if (q == "3")
+                {
+                    Console.WriteLine("Результат сложения = {0}", a + b);
+                }
+                if (q == "4")
+                {
+                    Console.WriteLine("Результат вычитания = {0}", a - b);
+                }
             }
         }
     }
